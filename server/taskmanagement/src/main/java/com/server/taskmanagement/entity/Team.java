0ca6da59216af9ee0e7c5d854adcd398d4f76b2c
burakerdgn1 <burakerdogan749@gmail.com>
2024-09-03 20:07:34 +0300

    Entity fixes.

--- server/taskmanagement/src/main/java/com/server/taskmanagement/entity/Team.java
+++ server/taskmanagement/src/main/java/com/server/taskmanagement/entity/Team.java
@@ -5,6 +5,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+import java.util.Set;
+
 @Entity
 @Table(name = "teams")
 @Data
@@ -15,19 +18,17 @@ public class Team {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
+  @Column(unique = true, nullable = false)
   private String name;
 
-  /*
   @OneToMany(mappedBy = "team", cascade = CascadeType.ALL, orphanRemoval = true)
   private List<UserTeam> userTeams;
-   */
 
-  /*
   @OneToMany(mappedBy = "team", cascade = CascadeType.ALL, orphanRemoval = true)
   private Set<Task> tasks;
 
   @OneToMany(mappedBy = "team", cascade = CascadeType.ALL, orphanRemoval = true)
   private Set<Project> projects;
-   */
+
 }
 
