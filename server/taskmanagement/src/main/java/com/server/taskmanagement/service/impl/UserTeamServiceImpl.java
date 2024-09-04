0b5af5323f623ea2dce858d4655e3eab2c9dc163
burakerdgn1 <burakerdogan749@gmail.com>
2024-09-04 23:45:15 +0300

    UserProject service is updated.

--- server/taskmanagement/src/main/java/com/server/taskmanagement/service/impl/UserTeamServiceImpl.java
+++ server/taskmanagement/src/main/java/com/server/taskmanagement/service/impl/UserTeamServiceImpl.java
@@ -1,9 +1,14 @@
 package com.server.taskmanagement.service.impl;
+import com.server.taskmanagement.entity.Team;
+import com.server.taskmanagement.entity.User;
 import com.server.taskmanagement.entity.UserTeam;
 import com.server.taskmanagement.repository.UserTeamRepository;
+import com.server.taskmanagement.service.interfaces.TeamService;
+import com.server.taskmanagement.service.interfaces.UserService;
 import com.server.taskmanagement.service.interfaces.UserTeamService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -11,12 +16,33 @@ import java.util.Optional;
 @Service
 @AllArgsConstructor
 public class UserTeamServiceImpl implements UserTeamService {
+  private final UserService userService;
+  private final TeamService teamService;
 
   private final UserTeamRepository userTeamRepository;
 
   @Override
-  public UserTeam addUserToTeam(UserTeam userTeam) {
-    return userTeamRepository.save(userTeam);
+  @Transactional
+  public void addUserToTeam(Long userId, Long teamId) {
+    User user = userService.findUserById(userId)
+      .orElseThrow(
+        //() -> new UserNotFoundException("User not found with id: " + userId)
+      );
+    Team team = teamService.findTeamById(teamId)
+      .orElseThrow(
+        //() -> new TeamNotFoundException("Team not found with id: " + teamId)
+      );
+
+    /*
+    if (userTeamRepository.existsByUserAndTeam(user, team)) {
+      throw new IllegalStateException("User is already a member of the team");
+    }
+     */
+
+    UserTeam userTeam = new UserTeam();
+    userTeam.setUser(user);
+    userTeam.setTeam(team);
+    userTeamRepository.save(userTeam);
   }
 
   @Override
--- server/taskmanagement/src/main/java/com/server/taskmanagement/service/interfaces/UserTeamService.java
+++ server/taskmanagement/src/main/java/com/server/taskmanagement/service/interfaces/UserTeamService.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface UserTeamService {
-  UserTeam addUserToTeam(UserTeam userTeam);
+  void addUserToTeam(Long userId, Long teamId);
 
   Optional<UserTeam> findUserTeamById(Long id);
 
