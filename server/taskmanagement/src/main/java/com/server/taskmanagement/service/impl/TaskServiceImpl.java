26c652f1f208d8a32de21cfcbe3e8f83c4dfd34b
burakerdgn1 <burakerdogan749@gmail.com>
2024-09-05 14:03:13 +0300

    Task service is updated.

--- server/taskmanagement/src/main/java/com/server/taskmanagement/service/impl/TaskServiceImpl.java
+++ server/taskmanagement/src/main/java/com/server/taskmanagement/service/impl/TaskServiceImpl.java
@@ -1,24 +1,26 @@
 package com.server.taskmanagement.service.impl;
 
 
+import com.server.taskmanagement.entity.Project;
 import com.server.taskmanagement.entity.Task;
 import com.server.taskmanagement.repository.TaskRepository;
+import com.server.taskmanagement.service.interfaces.ProjectService;
 import com.server.taskmanagement.service.interfaces.TaskService;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
 @Service
+@AllArgsConstructor
 public class TaskServiceImpl implements TaskService {
 
-  private final TaskRepository taskRepository;
+  private final ProjectService projectService;
 
-  @Autowired
-  public TaskServiceImpl(TaskRepository taskRepository) {
-    this.taskRepository = taskRepository;
-  }
+  private final TaskRepository taskRepository;
 
   @Override
   public Task createTask(Task task) {
@@ -54,6 +56,22 @@ public class TaskServiceImpl implements TaskService {
   @Override
   public void deleteTask(Long id) {
     taskRepository.deleteById(id);
+  }
+
+  @Override
+  @Transactional
+  public void addTaskToProject(Long taskId, Long projectId) {
+    Task task = taskRepository.findById(taskId)
+      .orElseThrow(
+        //() -> new TaskNotFoundException("Task not found with id: " + taskId)
+      );
+    Project project = projectService.findProjectById(projectId)
+      .orElseThrow(
+        //() -> new ProjectNotFoundException("Project not found with id: " + projectId)
+      );
+
+    task.setProject(project);
+    taskRepository.save(task);
   }
 }
 
