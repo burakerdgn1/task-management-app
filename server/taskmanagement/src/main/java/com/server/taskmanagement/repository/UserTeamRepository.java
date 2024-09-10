b75756ebe76689c13d1e78e3f350eb772c5eec34
burakerdgn1 <burakerdogan749@gmail.com>
2024-09-10 13:31:14 +0300

    GetUserTeamsByUserId function is added to UserTeam service.

--- server/taskmanagement/src/main/java/com/server/taskmanagement/repository/UserTeamRepository.java
+++ server/taskmanagement/src/main/java/com/server/taskmanagement/repository/UserTeamRepository.java
@@ -3,5 +3,8 @@ package com.server.taskmanagement.repository;
 import com.server.taskmanagement.entity.UserTeam;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UserTeamRepository extends JpaRepository<UserTeam, Long> {
+  List<UserTeam> findUserTeamsByUserId(Long userId);
 }
