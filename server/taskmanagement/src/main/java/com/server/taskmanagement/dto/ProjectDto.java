6748d548993dfc4e4f11fedc79bbbfd7267012c5
burakerdgn1 <burakerdogan749@gmail.com>
2024-09-11 15:39:51 +0300

    Project Dto is created.

--- server/taskmanagement/src/main/java/com/server/taskmanagement/dto/ProjectDto.java
+++ server/taskmanagement/src/main/java/com/server/taskmanagement/dto/ProjectDto.java
@@ -1,5 +1,6 @@
 package com.server.taskmanagement.dto;
 
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,7 +8,10 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class AuthRequest {
-  private String username;
-  private String password;
+public class ProjectDto {
+  private Long id;
+  private String name;
+  //private List<TaskDto> tasks;
+  //private List<TeamDto> teams;
 }
+
